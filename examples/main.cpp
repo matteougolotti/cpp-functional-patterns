40d894665f26d1687e6fdb125a4b3d1a686317d6
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2018-10-13 23:34:32 +0200

    Edited example code

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,6 +2,7 @@
 #include "../patterns.hpp";
 
 #include <cmath>
+#include <optional>
 
 int factorial(int n) {
   return fp::match<int, int>(n)
@@ -14,4 +15,18 @@ int fib(int n) {
     >= 0 > 1
     >= 1 > 1
     |      [] (int n) { return fib (n - 1) + fib (n - 2); };
+}
+
+enum Number {
+  ONE,
+  TWO,
+  THREE
+};
+
+std::optional<std::string> toString(Number n) {
+  return fp::match<Number, std::optional<std::string>>(n)
+    >= ONE   > std::optional<std::string>{"One"}
+    >= TWO   > std::optional<std::string>{"Two"}
+    >= THREE > std::optional<std::string>{"Three"}
+    |          std::nullopt;
 }
